9bc17e04f46f7bf04ee815541a34c07525e5e97d
Brittni Watkins <blwatkins@users.noreply.github.com>
2014-10-29 15:17:56 -0500

    Added VerletOrg.h file

---
+++ VerletOrg.h
@@ -0,0 +1,31 @@
+#ifndef __Verlet_Organisms__VerletOrg__
+#define __Verlet_Organisms__VerletOrg__
+
+#include "ofMain.h"
+#include "VerletStick.h"
+#include "VerletBall.h"
+
+namespace CSE1342 {
+	class VerletOrg {
+		private:
+			CSE1342::VerletStick* sticks;
+			CSE1342::VerletBall* balls;
+			int stickNum;
+			int ballNum;
+			void _init();
+	
+		public:
+			VerletOrg();
+			VerletOrg(int stickNum, int ballNum);
+			VerletOrg(const VerletOrg& v);
+			~VerletOrg();
+			VerletOrg& operator=(const VerletOrg& v);
+	
+			virtual void position();
+			virtual void connect();
+			virtual void push();
+			void display();
+			void move();
+	};
+}
+#endif

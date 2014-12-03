001ba2cecff74cd976fd4a47001de199301531f2
katfield <khatfield@smu.edu>
2014-12-03 16:42:44 -0600

    Create Celery.h

---
+++ ClickClickMadness src/Celery.h
@@ -0,0 +1,26 @@
+//
+//  Celery.h
+//  3D_example
+//
+//  Created by Kaitlyn on 12/3/14.
+//
+//
+
+#ifndef ___D_example__Celery__
+#define ___D_example__Celery__
+
+#include <stdio.h>
+#include "Base3D.h"
+
+class Celery : public Base3D {
+    
+public:
+    Celery();
+    Celery (const ofVec3f& loc, const ofVec3f& sz, const ofColor& col, const Light& lt);
+    
+    
+private:
+    void _init();
+    
+};
+#endif /* defined(___D_example__Celery__) */

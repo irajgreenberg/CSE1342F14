d438a837befe788940827edc795997580b1e8e56
katfield <khatfield@smu.edu>
2014-12-08 15:10:14 -0600

    Update Celery.cpp
    The lighting should be much better!

--- ClickClickMadness src/Celery.cpp
+++ ClickClickMadness src/Celery.cpp
@@ -19,6 +19,7 @@ Base3D(loc, sz, col, lt){
 
 void Celery::_init(){
     //top of donut
+    
     vecs.push_back(ofVec3f(0, -.5, 0));
     vecs.push_back(ofVec3f(-.25, -.5, -.75));
     vecs.push_back(ofVec3f(-.75, -.5, -.25));
@@ -30,6 +31,7 @@ void Celery::_init(){
     vecs.push_back(ofVec3f(.25, -.5, -.75));
     
     //bottom of donut
+    
     vecs.push_back(ofVec3f(-.25, .5, -.75));
     vecs.push_back(ofVec3f(-.75, .5, -.25));
     vecs.push_back(ofVec3f(-.75, .5, .25));
@@ -42,16 +44,18 @@ void Celery::_init(){
     
     
     //top
-    indices.push_back(Index<int>(0, 2, 1 ));
-    indices.push_back(Index<int>(0, 3, 2 ));
-    indices.push_back(Index<int>(0, 4, 3 ));
-    indices.push_back(Index<int>(0, 5, 4));
-    indices.push_back(Index<int>(0, 6, 5));
-    indices.push_back(Index<int>(0, 7, 6));
-    indices.push_back(Index<int>(0, 8, 7));
-    indices.push_back(Index<int>(0, 1, 8));
+    
+    indices.push_back(Index<int>(0, 1, 2 ));
+    indices.push_back(Index<int>(0, 2, 3 ));
+    indices.push_back(Index<int>(0, 3, 4 ));
+    indices.push_back(Index<int>(0, 4, 5));
+    indices.push_back(Index<int>(0, 5, 6));
+    indices.push_back(Index<int>(0, 6, 7));
+    indices.push_back(Index<int>(0, 7, 8));
+    indices.push_back(Index<int>(0, 8, 1));
     
     //bottom
+    
     indices.push_back(Index<int>(17, 9, 16));
     indices.push_back(Index<int>(17, 16, 15));
     indices.push_back(Index<int>(17, 15, 14));
@@ -61,26 +65,29 @@ void Celery::_init(){
     indices.push_back(Index<int>(17, 11, 10));
     indices.push_back(Index<int>(17, 10, 9));
     
+    
     //sides with two bottom one top
-    indices.push_back(Index<int>(13, 12, 4));
-    indices.push_back(Index<int>(12, 11, 3));
-    indices.push_back(Index<int>(11, 10, 2));
-    indices.push_back(Index<int>(10, 9, 1));
-    indices.push_back(Index<int>(9, 16, 8));
-    indices.push_back(Index<int>(16, 15, 7));
-    indices.push_back(Index<int>(15, 14, 6));
-    indices.push_back(Index<int>(14, 13, 5));
+    
+    indices.push_back(Index<int>(13, 4, 12));
+    indices.push_back(Index<int>(12, 3, 11));
+    indices.push_back(Index<int>(11, 2, 10));
+    indices.push_back(Index<int>(10, 1, 9));
+    indices.push_back(Index<int>(9, 8, 16));
+    indices.push_back(Index<int>(16, 7, 15));
+    indices.push_back(Index<int>(15, 6, 14));
+    indices.push_back(Index<int>(14, 5, 13));
     
     //sides with two top one bottom
-    indices.push_back(Index<int>(4, 5, 13));
-    indices.push_back(Index<int>(3, 4, 12));
-    indices.push_back(Index<int>(2, 3, 11));
-    indices.push_back(Index<int>(1, 2, 10));
-    indices.push_back(Index<int>(8, 1, 9));
-    indices.push_back(Index<int>(7, 8, 16));
-    indices.push_back(Index<int>(6, 7, 15));
-    indices.push_back(Index<int>(5, 6, 14));
-	
+    
+    indices.push_back(Index<int>(4, 13, 5));
+    indices.push_back(Index<int>(3, 12, 4));
+    indices.push_back(Index<int>(2, 11, 3));
+    indices.push_back(Index<int>(1, 10, 2));
+    indices.push_back(Index<int>(8, 9, 1));
+    indices.push_back(Index<int>(7, 16, 8));
+    indices.push_back(Index<int>(6, 15, 7));
+    indices.push_back(Index<int>(5, 14, 6));
+
     
     for (int i=0; i<indices.size(); ++i){
         // to do
